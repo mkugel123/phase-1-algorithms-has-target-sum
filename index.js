8ff6d8f1a2ae054d53c9d5326273c8190c5e5c58
Zalman <moishkugel@gmail.com>
2022-09-11 12:31:09 -0400

    commit tree 1a384accd5149f03a5386f3e50841345cf73eb61

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const arrayOfSums = []
+  array.forEach((number) => {
+    let numberIndex = array.indexOf(number)
+    for (let i = 0; i < array.length; i++){
+      let otherNumber = array[i]
+      if (i !== numberIndex){
+        arrayOfSums.push(number + otherNumber)
+      }
+    }
+  })
+  return arrayOfSums.includes(target)
 }
 
 /* 
   Write the Big O time complexity of your function here
 */
 
+/*
+  Add each number of the array to all the other numbers
+  if any of the sums equal to the other number given return true
+*/
+
 /* 
   Add your pseudocode here
+  for each number in array
+    add each other number to it besides for the number itself
+    and push sum into another array
+     compare numbers of new array to target
 */
 
 /*
   Add written explanation of your solution here
+
+  for each number in the array that was used as argument,
+  a for loop is used to add it to all other numbers besides
+  for itself and the sums are pushed into arrayOfSums.
+  Then array.includes() method is used to return true if
+  arrayOfSums includes target.
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,15 +46,17 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
-  console.log("");
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([12, 14, 25], 25))
 
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
 
-  console.log("");
-
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 2, 2, 5], 4));
 }
 
 module.exports = hasTargetSum;
